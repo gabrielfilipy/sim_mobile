927e719d5a241d1fc8bb28422f63898c86515665
gabrielfilipy <gabrielfilipycorrea@gmail.com>
2024-08-02 20:10:14 -0400

    Projeto base.

---
+++ Style.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  Text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  }
+});
+
+export default styles;
