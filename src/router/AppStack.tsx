c6cf0c92212242f863e69d6856ca68ab1db27317
elielalvespinheiro <elielalvespinheiro3456@gmail.com>
2024-08-19 18:21:41 -0400

    pequena correcao nas rotas

--- src/router/AppStack.tsx
+++ src/router/AppStack.tsx
@@ -2,14 +2,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { HomeScreen } from "../screen/Home";
 import { Mesa } from "../screen/mesa/Mesa";
+import { RootStackParamList } from "./types";
 
-const Stack = createNativeStackNavigator();
-
+//Cria um stack navigator que gerencia a navegação entre telas empilhadas
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function AppStack() {
 
+    //Stack.Navigator e Stack.Screen: 
+    //Configuram as rotas do stack navigator 
+    //são as rotas disponíveis dentro desse stack navigator.
+
     return (
-        <Stack.Navigator>
+    <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Mesa" component={Mesa} />
     </Stack.Navigator>
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import { AppStack } from "./AppStack";
 import { AuthStack } from "./AuthStack";
 
-const auth = true;
+const auth = true; //autentica a autorizacao do usuario. 
 
 export function Router() {
     return (
+
+        //muito parecido com o app, mas aqui separei entre uma rota que leva para uma tela de login 
+        //e outra para a aplicação em si.
+
         <NavigationContainer>
             {auth ? <AppStack /> : <AuthStack />}
         </NavigationContainer>
